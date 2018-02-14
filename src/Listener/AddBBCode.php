0cbf9c88eec6250f416e18f4e652cb3c470a7a73
skywalker <houzhenhong@hotmail.com>
2018-02-14 12:30:01 +0800

    add bbcode

---
+++ src/Listener/AddBBCode.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * (c) Sajjad Hashemian <wolaws@gmail.com>
+ */
+namespace GaNuongLaChanh\MarkdownEditor\Listener;
+
+use Flarum\Event\ConfigureFormatter;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class AddBBCode
+{
+    /**
+     * @param Dispatcher $events
+     */
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(ConfigureFormatter::class, [$this, 'addBBCode']);
+    }
+    /**
+     * @param ConfigureFormatter $event
+     */
+    public function addBBCode(ConfigureFormatter $event)
+    {
+        // $event->configurator->BBCodes->addFromRepository('SPOILER');
+        $event->configurator->BBCodes->addCustom(
+            '[SPOILER]{TEXT}[/SPOILER]',
+            '<div class="spoiler">{TEXT}</div>'
+        );
+    }
+}
