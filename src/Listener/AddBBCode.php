f86a46a85a79969aec5652c03f42e68037bc867a
skywalker <houzhenhong@hotmail.com>
2018-02-15 10:06:53 +0800

    beautify codes

--- src/Listener/AddBBCode.php
+++ src/Listener/AddBBCode.php
@@ -50,67 +50,56 @@ class AddBBCode
         $event->configurator->BBCodes->addCustom(             
             '[SIZE2]{TEXT}[/SIZE2]',             
             '<span style="font-size: 15px;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('SIZE3');
         $event->configurator->BBCodes->addCustom(             
             '[SIZE3]{TEXT}[/SIZE3]',             
             '<span style="font-size: 22px;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('SIZE4');
         $event->configurator->BBCodes->addCustom(             
             '[SIZE4]{TEXT}[/SIZE4]',             
             '<span style="font-size: 26px;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORT');
         $event->configurator->BBCodes->addCustom(             
             '[COLORT]{TEXT}[/COLORT]',             
             '<span style="color: #1abc9c;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORG');
         $event->configurator->BBCodes->addCustom(             
             '[COLORG]{TEXT}[/COLORG]',             
             '<span style="color: #2ecc71;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORB');
         $event->configurator->BBCodes->addCustom(             
             '[COLORB]{TEXT}[/COLORB]',             
             '<span style="color: #3498db;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORP');
         $event->configurator->BBCodes->addCustom(             
             '[COLORP]{TEXT}[/COLORP]',             
             '<span style="color: #9b59b6;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORY');
         $event->configurator->BBCodes->addCustom(             
             '[COLORY]{TEXT}[/COLORY]',             
             '<span style="color: #f1c40f;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORO');
         $event->configurator->BBCodes->addCustom(             
             '[COLORO]{TEXT}[/COLORO]',             
             '<span style="color: #e67e22;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORR');
         $event->configurator->BBCodes->addCustom(            
             '[COLORR]{TEXT}[/COLORR]',             
             '<span style="color: #e74c3c;">{TEXT}</span>'
-
         );
         //$event->configurator->BBCodes->addFromRepository('COLORS');
         $event->configurator->BBCodes->addCustom(             
             '[COLORS]{TEXT}[/COLORS]',             
             '<span style="color: #95a5a6;">{TEXT}</span>'
-
         );
     }
 }
